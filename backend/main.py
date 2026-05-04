82fd54b7ca4d483f0f4ad024f4530e9ccf5c8cfc
Théo Hénon <theo.henon@epita.fr>
2026-05-04 13:32:29 +0200

    feat: create a basic CLI

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
+import argparse
 import os
-import sys
 
 import rich.traceback
 from PIL import Image
@@ -9,15 +9,34 @@ from src.table_detector import DetrTableDetector
 rich.traceback.install()
 
 
-def main(argv: list[str]) -> None:
-    if len(argv) != 2:
-        print("Usage: python main.py <IMAGE_PATH>")
-        sys.exit(1)
+def parse_cli() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Detect tables in an image")
+    parser.add_argument("image_path", help="Path to the input image")
+    parser.add_argument("output_dir", help="Path to the output directory")
+    parser.add_argument(
+        "--threshold", type=float, default=0.75, help="Confidence threshold for detections"
+    )
+    parser.add_argument(
+        "--annotate",
+        action="store_true",
+        help="Whether to annotate the image with detections",
+        default=True,
+    )
+    parser.add_argument(
+        "--pretrained_model",
+        type=str,
+        default="TahaDouaji/detr-doc-table-detection",
+        help="Pretrained model to use for table detection. Default is ",
+    )
+    return parser.parse_args()
 
-    image_path = argv[1]
-    image = Image.open(image_path)
 
-    table_detector = DetrTableDetector(threshold=0.75, annotate=False)
+def main() -> None:
+    args = parse_cli()
+
+    image = Image.open(args.image_path)
+
+    table_detector = DetrTableDetector(threshold=args.threshold, annotate=args.annotate)
     scores, labels, boxes, annotated_image = table_detector.detect_tables(image)
 
     for score, label, box in zip(scores, labels, boxes, strict=False):
@@ -30,11 +49,11 @@ def main(argv: list[str]) -> None:
     # Save the image with detections
     if table_detector.annotate:
         output_path = os.path.join(
-            "..", "data", "output", f"{os.path.basename(image_path).split('.')[0]}-annotated.png"
+            args.output_dir, f"{os.path.basename(args.image_path).split('.')[0]}-annotated.png"
         )
         annotated_image.save(output_path)
         print(f"Annotated image saved to {output_path}")
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
